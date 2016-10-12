2f99a8f4f26d673d96bbd8f101872c13157af157
Di <pdv.spb@gmail.com>
2016-10-12 17:02:23 +0300

    Some more User modesl specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,6 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email) }
+  it { should validate_length_of(:name).is_at_least(3) }
+  it { should_not allow_value("de@nis").for(:email) }
 end
