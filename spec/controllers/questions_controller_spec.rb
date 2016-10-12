f4bf9914337d00aaa967d63791ce0c9fdf212b9d
Di <pdv.spb@gmail.com>
2016-10-12 22:51:34 +0300

    Question controller generated

---
+++ spec/controllers/questions_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe QuestionsController, type: :controller do
+  describe 'GET #index' do
+    it 'populates an array of all questions' do
+      
+    end
+  end
+
+end
