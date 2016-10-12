52833b8a95d38e7d5332a60341e293ac081904b4
Di <pdv.spb@gmail.com>
2016-10-12 16:32:16 +0300

    User model

---
+++ db/migrate/20161012132021_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :name
+
+      t.timestamps null: false
+    end
+    
+    add_index :users, :email, unique: true
+    add_index :users, :name
+  end
+end
