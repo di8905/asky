417324c6528194f68802fb4fbc9647d8b6e741ba
Di <pdv.spb@gmail.com>
2016-10-12 22:06:32 +0300

    User references tests pass

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,7 @@
 class Question < ActiveRecord::Base
+  has_many :answers, dependent: :destroy
+  belongs_to :user
+  
   validates :title, presence: true, length: { minimum: 5 }
   validates :body, presence: true, length: {minimum: 10}
-  
-  has_many :answers, dependent: :destroy
 end
