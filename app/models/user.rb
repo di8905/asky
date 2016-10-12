41d46263fb57ac949eafbfed2f95a9c8be3e06b2
Di <pdv.spb@gmail.com>
2016-10-12 22:06:32 +0300

    User references tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   EMAIL = /.+@.+\..+/i
   
+  has_many :questions
+  has_many :answers
+  
   validates :name, :email, presence: true
   validates :email, uniqueness: { case_sensitive: false }
   validates :name, length: { minimum: 3 }
