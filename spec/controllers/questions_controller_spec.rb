105b1c7a2d009d2a9ad6681c7ab21ad4126338f3
Di <pdv.spb@gmail.com>
2016-10-13 15:51:44 +0300

    [A

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
+  let(:question) { FactoryGirl.create(:question) }
+  
   describe 'GET #index' do
     let(:questions) { FactoryGirl.create_list(:question, 3) }
     before { get :index }
@@ -15,7 +17,6 @@ RSpec.describe QuestionsController, type: :controller do
   end
   
   describe 'GET #show' do
-    let(:question) { FactoryGirl.create(:question) }
     before { get :show, id: question }
     
     it 'assigns to @question variable appropriate question object' do
@@ -24,6 +25,44 @@ RSpec.describe QuestionsController, type: :controller do
     
     it 'renders the view show' do
       expect(response).to render_template :show
+    end
+  end
+  
+  describe 'GET #new' do
+    before { get :new}
+    
+    it 'assings new Question to @question variable' do
+      expect(assigns(:question)).to be_a_new(Question) 
+    end
+    
+    it 'renders the view new' do
+      expect(response).to render_template :new
+    end
+  end
+  
+  describe 'GET #edit' do
+    before { get :edit, id: question }
+    
+    it 'assigns to @question variable appropriate question object' do
+      expect(assigns(:question)).to eq question 
+    end
+    
+    it 'renders the view edit' do 
+      expect(response).to render_template :edit
+    end
+  end
+  
+  describe "POST #create" do
+    context 'with valid attributes' do 
+      it 'saves new question in database' do
+        expect { post :create, question: FactoryGirl.attributes_for(:question) }.to change(Question, :count).by(1)
+        post :create, question: FactoryGirl.attributes_for(:question)
+      end
+      
+      it 'redirects to show view' do
+        post :create, question: FactoryGirl.attributes_for(:question)
+        expect(response).to redirect_to question_path(assigns(:question))
+      end
     end
   end
 
