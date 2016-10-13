590d2800bd5fe5f3de6d5f43e20f847bc69e3060
Di <pdv.spb@gmail.com>
2016-10-13 21:18:06 +0300

    Question CRUD controller tests pass

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,4 +14,34 @@ class QuestionsController < ApplicationController
   def edit
     @question = Question.find(params[:id])
   end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save 
+      redirect_to @question
+    else 
+      render :new
+    end
+  end
+  
+  def update
+    @question = Question.find(params[:id])
+    if @question.update(question_params) 
+      redirect_to @question
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy 
+    redirect_to questions_path
+  end
+  
+  private  
+  def question_params
+    params.require(:question).permit(:title, :body)
+  end
+
 end
