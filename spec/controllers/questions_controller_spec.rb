315389b955a4e55f17acc0307f6ee513ef40c8d9
Di <pdv.spb@gmail.com>
2016-10-13 14:07:06 +0300

    Show action test pass

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -3,16 +3,27 @@ require 'rails_helper'
 RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { FactoryGirl.create_list(:question, 3) }
-    before do
-      get :index
-    end
-    
+    before { get :index }
+
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
     end
     
     it 'renders index view' do
       expect(response).to render_template :index
+    end
+  end
+  
+  describe 'GET #show' do
+    let(:question) { FactoryGirl.create(:question) }
+    before { get :show, id: question }
+    
+    it 'assigns to @question variable appropriate question object' do
+      expect(assigns(:question)).to eq question 
+    end
+    
+    it 'renders the view show' do
+      expect(response).to render_template :show
     end
   end
 
