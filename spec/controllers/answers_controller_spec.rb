324b99c412da38e4c5b9adafc7d9f04c4f3cf19c
Di <pdv.spb@gmail.com>
2016-10-14 13:34:41 +0300

    Answer create tests pass

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { FactoryGirl.create(:question) }
-  before { get :new, question_id: question.id }
 
   describe 'GET #new' do
+    before { get :new, question_id: question.id }
+    
     it 'assigns appropriate @question variable to make new answer' do
       expect(assigns(:question)).to eq question
     end
@@ -22,13 +23,31 @@ RSpec.describe AnswersController, type: :controller do
     end
   end
       
-  # describe 'POST #create' do
-  #   context 'with valid attributes' do
-  #     it 'saves new answer in database' do
-  #       question
-  #       expect { post :create, question_id: question.id, answer: FactoryGirl.attributes_for(:answer) }.to change(question.answers, :count).by(1)
-  #     end
-  #   end
-  # end
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'saves new answer in database' do
+        expect { post :create, question_id: question.id, answer: FactoryGirl.attributes_for(:answer) }.to change(question.answers, :count).by(1)
+      end
+      
+      it 'redirects to matching question' do
+        post :create, question_id: question.id, answer: FactoryGirl.attributes_for(:answer)
+        expect(response).to redirect_to question
+      end
+    end
+    
+    context 'with invalid attributes' do
+      let(:invalid_attrbutes) { FactoryGirl.attributes_for(:invalid_answer) }
+      
+      it 'does not save invalid answer to database' do
+        expect { post :create, question_id: question.id, answer: invalid_attrbutes }.not_to change(question.answers, :count)
+      end
+      
+      it 'renders the new view' do
+        post :create, question_id: question.id, answer: invalid_attrbutes
+        expect(response).to render_template :new
+      end
+      
+    end
+  end
 
 end
