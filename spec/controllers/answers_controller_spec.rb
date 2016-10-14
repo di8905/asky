f5ba5c45c0b53f2d0cdcbaf5b690eeb446f7c71c
Di <pdv.spb@gmail.com>
2016-10-14 13:08:35 +0300

    Answers#new refactoring

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,25 +2,22 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { FactoryGirl.create(:question) }
+  before { get :new, question_id: question.id }
 
   describe 'GET #new' do
     it 'assigns appropriate @question variable to make new answer' do
-      get :new, question_id: question.id
       expect(assigns(:question)).to eq question
     end
     
     it 'assigns new Answer to @answer variable' do
-      get :new, question_id: question.id
       expect(assigns(:answer)).to be_a_new(Answer)
     end
     
     it 'is associated with @question' do
-      get :new, question_id: question.id
       expect(assigns(:answer).question).to eq question
     end
     
     it 'render view :new' do
-      get :new, question_id: question.id
       expect(response).to render_template :new
     end
   end
