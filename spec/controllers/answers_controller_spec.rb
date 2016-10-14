d02988b9679357796ec840801b7c1e7d15634b7d
Di <pdv.spb@gmail.com>
2016-10-14 16:05:01 +0300

    Answer controller tests done

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe AnswersController, type: :controller do
   let(:question) { FactoryGirl.create(:question) }
+  let(:answer) { question.answers.create(FactoryGirl.attributes_for(:answer)) }
 
   describe 'GET #new' do
     before { get :new, question_id: question.id }
@@ -39,16 +40,60 @@ RSpec.describe AnswersController, type: :controller do
     end
     
     context 'with invalid attributes' do
-      
       it 'does not save invalid answer to database' do
-        expect { invalid_answer_action }.not_to change(question.answers, :count)
+        expect { invalid_answer_action }.not_to change(Answer, :count)
       end
       
       it 'renders the new view' do
         invalid_answer_action
         expect(response).to render_template :new
       end
+    end
+  end
+  
+    
+  describe 'PATCH #update' do
+    context 'with valid attributes' do
+      before do
+        patch :update, question_id: question.id, id: answer, answer: { body: "Custom not factored answer" }
+      end
       
+      it 'updates answer' do
+        answer.reload
+        expect(answer.body).to eq "Custom not factored answer"
+      end
+      
+      it 'redirects to question' do
+        expect(response).to redirect_to question
+      end 
+    end
+    
+    context 'with invalid attributes' do
+      before do
+        patch :update, question_id: question.id, id: answer, answer: { body: nil }
+      end
+      
+      it 'does not update answer' do
+        answer.reload
+      expect(answer.body).to eq 'My text'
+      end
+      
+      it 're-renders form edit' do
+        expect(response).to render_template :new
+      end
+    end
+  end
+  
+  describe 'DELETE #delete' do
+    let(:delete_action) { delete :destroy, question_id: question.id, id: answer }
+    it 'deletes the answer' do
+      answer
+      expect { delete_action }.to change(Answer, :count).by(-1)
+    end
+    
+    it 'redirects to question' do
+      delete_action
+      expect(response).to redirect_to question
     end
   end
 
