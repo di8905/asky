0bf16d8746e62bc233b968a5b2d6387a4fbefa0c
Di <pdv.spb@gmail.com>
2016-10-17 16:51:49 +0300

    Login and logout specs pass

--- spec/acceptance/sign_in_spec.rb
+++ spec/acceptance/sign_in_spec.rb
@@ -3,16 +3,11 @@ require 'rails_helper'
 feature 'User sign in', %q{
   In order to be able to ask a question as 
   an user, I want to be able to login
-} do
+} do  
+  given(:user) { FactoryGirl.create(:user) } 
   
   scenario 'Registered user trying to sign in' do
-    User.create!(email: 'user@test.com', name: "John", password: '12345678')
-    
-    visit new_user_session_path
-    fill_in 'Email', with: 'user@test.com'
-    fill_in 'Password', with: '12345678'
-    click_on 'Sign in'
-    
+    log_in(user)
     expect(page).to have_content 'Signed in successfully'
     expect(current_path).to eq root_path
   end
@@ -21,17 +16,25 @@ feature 'User sign in', %q{
     visit new_user_session_path
     fill_in 'Email', with: 'wrong_email@test.com'
     fill_in 'Password', with: 'qqqqqqqq'
+    click_on 'Log in'
     
-    expect(page).to have_content 'Invalid email or password'
+    expect(page).to have_content 'Invalid Email or password.'
     expect(current_path).to eq new_user_session_path
   end
-
-  
-  
-  
-  
-  
-  
-  
-  
 end
+
+feature 'User Log out', %q{
+  In order to able end session user can be able
+  to Log-out
+} do
+  given(:user) { FactoryGirl.create(:user) } 
+
+  scenario 'Logged-in user clicks logout button' do
+    log_in(user)
+    click_on 'Log out'
+    
+    expect(page).to have_content 'Signed out successfully.'    
+    expect(current_path).to eq root_path
+  end
+  
+end    
