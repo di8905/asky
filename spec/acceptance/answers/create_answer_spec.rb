a4e9dbd7c44feef430fa34e8d1de95205207c8fb
Di <pdv.spb@gmail.com>
2016-10-23 19:47:13 +0300

    Answer creation js works

--- spec/acceptance/answers/create_answer_spec.rb
+++ spec/acceptance/answers/create_answer_spec.rb
@@ -4,19 +4,18 @@ feature 'create answer', %q{
   User can create answer while in question page
 } do 
   
-  scenario 'authenticated user creates the answer' do
+  scenario 'authenticated user creates the answer', js: true do
     question = FactoryGirl.create(:question)
     log_in(question.user)
     visit question_path(question)
     fill_in 'Answer:', with: 'My test answer'
     click_on('Post your answer')
     
-    expect(page).to have_content('Your answer have been successfully added')
-    expect(page).to have_content('My test answer')
+    within('.answers') { expect(page).to have_content('My test answer') }
     expect(current_path).to eq question_path(question)
   end
   
-  scenario 'unauthenticated user tries to create the answer' do
+  scenario 'unauthenticated user tries to create the answer', js: true do
     question = FactoryGirl.create(:question)
     visit question_path(question)
     fill_in 'Answer:', with: 'My test answer'
@@ -25,14 +24,14 @@ feature 'create answer', %q{
     expect(page).to have_content('You need to sign in or sign up before continuing.')
   end
   
-  scenario 'authenticated user creates the invalid answer' do
+  scenario 'authenticated user creates the invalid answer', js: true do
     question = FactoryGirl.create(:question)
     log_in(question.user)
     visit question_path(question)
     fill_in 'Answer:', with: 'My'
     click_on('Post your answer')
     
-    expect(page).to have_content('Errors Answer')
+    expect(page).to have_content('Validation failed: Body is too short')
   end
   
 end
