76cf5bc72d55b8185e83a32beb720e41aa3579dd
Di <pdv.spb@gmail.com>
2016-11-21 08:58:55 +0300

    Vote refactor

--- app/models/concerns/voteable.rb
+++ app/models/concerns/voteable.rb
@@ -2,34 +2,30 @@ module Voteable
   extend ActiveSupport::Concern
   included do
     has_many :votes, as: :voteable, dependent: :destroy
-    
-    def vote(user, vote)
-      if self.user.id == user.id
-        self.errors.add(:base, "Can't vote your own!")
-      else  
-        vote.to_i > 0 ? vote = 1 : vote = -1
-        process_vote(user, vote)
-      end
+  end
+  
+  def vote(user, vote)
+    if self.user.id == user.id
+      self.errors.add(:base, "Can't vote your own!")
+    else  
+      vote.to_i > 0 ? vote = 1 : vote = -1
+      process_vote(user, vote)
     end
-    
-    def rating
-      sum = 0
-      self.votes.each do |record|
-        sum += record.vote
-      end
-      sum
-    end
-    
-    private
-    
-    def process_vote(user, vote)
-      vote_collection = self.votes.where(user_id: user.id)
-      if vote_collection.empty?
-        self.votes.new(user_id: user.id, vote: vote).save!
-      else
-        self.errors.add(:base, 'Already voted this!') if vote_collection.first.vote == vote
-        vote_collection.first.destroy! if vote_collection.first.vote + vote = 0
-      end
+  end
+  
+  def rating
+    self.votes.reload.sum(:vote)
+  end
+  
+  private
+  
+  def process_vote(user, vote)
+    vote_collection = self.votes.where(user_id: user.id)
+    if vote_collection.empty?
+      self.votes.new(user_id: user.id, vote: vote).save!
+    else
+      self.errors.add(:base, 'Already voted this!') if vote_collection.first.vote == vote
+      vote_collection.first.destroy! if vote_collection.first.vote + vote = 0
     end
   end
 end
