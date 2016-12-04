1cfe2fa91b5df0fc5e8ddcd07d8b479afb5a695d
Di <pdv.spb@gmail.com>
2016-12-04 15:47:57 +0300

    Callback controller refactor

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,17 +1,18 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
-    if @user && @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Facebook') if is_navigational_format?
-    end
+    common_callback('Facebook')    
   end
   
   def twitter
+    common_callback('Twitter')
+  end
+  
+  private
+  def common_callback(provider)
     @user = User.find_for_oauth(request.env['omniauth.auth'])
     if @user && @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: 'Twitter') if is_navigational_format?
+      set_flash_message(:notice, :success, kind: provider) if is_navigational_format?
     end
   end
 end
