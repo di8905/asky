c275519df619c6f23fa30aaa2174dbddca7e8138
Di <pdv.spb@gmail.com>
2016-12-08 22:52:07 +0300

    Users profiles api

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resource :profiles do
         get :me, on: :collection
+        get :others, on: :collection
       end
     end
   end
--- spec/api/v1/profiles_spec.rb
+++ spec/api/v1/profiles_spec.rb
@@ -39,4 +39,56 @@ describe 'Profile API' do
       end
     end
   end
+  
+  describe 'GET /others' do
+    context 'unauthorized' do
+      it 'returns 401 status if no access token' do
+        get '/api/v1/profiles/others', format: :json
+        
+        expect(response.status).to eq 401
+      end
+      
+      it 'returns 401 status if access token invalid' do
+        get '/api/v1/profiles/others', format: :json, access_token: '1234'
+        
+        expect(response.status).to eq 401
+      end
+    end
+    
+    context 'authorized' do
+      let(:me) { FactoryGirl.create(:user) }
+      let!(:others) { FactoryGirl.create_list(:user, 3) }
+      let(:access_token) { FactoryGirl.create(:access_token, resource_owner_id: me.id) }
+      
+      before { get '/api/v1/profiles/others', format: :json, access_token: access_token.token }
+      
+      it 'returns 200 status' do
+        expect(response).to be_success
+      end
+      
+      it 'contains users profiles except me' do
+        expect(response.body).to be_json_eql(others.to_json)
+      end
+      
+      it 'contains users profiles except me' do
+        expect(response.body).to_not include_json(me.to_json)
+      end
+      
+      %w(email id created_at updated_at admin name).each do |attr|
+        it "each record contains attribute #{attr}" do
+          others.each_with_index do |user, i| 
+            expect(response.body).to be_json_eql(user.send(attr.to_sym).to_json).at_path("#{i}/#{attr}")
+          end
+        end
+      end
+      
+      %w(password encrypted_password).each do |attr|
+        it "each user does not contain attribute #{attr}" do
+          others.each_index do |i|
+            expect(response.body).not_to have_json_path("#{i}/#{attr}")
+          end
+        end
+      end
+    end
+  end
 end
