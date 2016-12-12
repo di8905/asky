5326ea70ca4a8ad2b4b74f5d77ad8ffe77e88bb1
Di <pdv.spb@gmail.com>
2016-12-12 12:12:40 +0300

    Answer show spec pass

--- app/controllers/api/v1/answers_controller.rb
+++ app/controllers/api/v1/answers_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::AnswersController < Api::V1::BaseController
   authorize_resource
-  before_action :set_question
+  before_action :set_question, only: :index
     
   def index
     @answers = @question.answers
     respond_with @answers
+  end
+  
+  def show
+    @answer = Answer.find(params[:id])
+    respond_with @answer
   end
   
   private
--- spec/api/v1/answers_spec.rb
+++ spec/api/v1/answers_spec.rb
@@ -22,6 +22,7 @@ describe 'Answers API' do
     
     context 'authorized' do
       let!(:access_token) { FactoryGirl.create(:access_token) }
+      
       before { get "/api/v1/questions/#{question.id}/answers", format: :json, access_token: access_token.token }
       
       it 'returns question answers list' do
@@ -34,5 +35,36 @@ describe 'Answers API' do
         end
       end
     end
+  end
+  
+  describe "Get /show" do
+    let!(:answer) { FactoryGirl.create(:answer) }
+    # let!(:question) { answer.question }
+
+    context 'unauthorized' do
+      it 'returns 401 status then there is no access token' do
+        get "/api/v1/answers/#{answer.id}", format: :json
+        
+        expect(response.status).to eq 401
+      end
+      
+      it 'returns 401 status then access token is invalid' do
+        get "/api/v1/answers/#{answer.id}", format: :json, access_token: '123456'
+        
+        expect(response.status).to eq 401
+      end
+    end
+    
+    context 'authorized' do
+      let!(:access_token) { FactoryGirl.create(:access_token) }
+      before { get "/api/v1/answers/#{answer.id}", format: :json, access_token: access_token.token }
+      
+      %w(id rating created_at updated_at body question_id user_id best).each do |attr|
+        it "has attribite #{attr}" do
+          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("answer/#{attr}")
+        end
+      end
+    end
+    
   end
 end
