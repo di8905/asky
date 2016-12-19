42c53be38bfaaa68b6ea0249c126a346e5c0b9d4
Di <pdv.spb@gmail.com>
2016-12-19 21:31:49 +0300

    User list patch up

--- spec/jobs/send_answer_newsletter_job_spec.rb
+++ spec/jobs/send_answer_newsletter_job_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe SendAnswerNewsletterJob, type: :job do
-  let(:answer) { FactoryGirl.create(:answer) }
-  before { FactoryGirl.create(:subscription) }
+  let(:question) { FactoryGirl.create(:question) }
+  let(:answer) { FactoryGirl.create(:answer, question: question) }
+  let(:users) { FactoryGirl.create_list(:user, 3) }
+  before do
+    question.subscribers << users
+  end
   
   it 'should mail answer to users' do
-    expect(AnswerMailer).to receive(:answer_update).and_call_original
+    question.subscribers.find_each.each do |user|
+      expect(AnswerMailer).to receive(:answer_update).with(user, answer).and_call_original
+    end
     
     SendAnswerNewsletterJob.new.perform(answer)
   end
