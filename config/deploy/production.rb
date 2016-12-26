6830152cdf29382e9e519750c3f9e064b2feaf2d
Di <pdv.spb@gmail.com>
2016-12-26 15:35:22 +0300

    Server name change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '192.168.10.101', user: 'deployer', roles: %w{web app db}, primary: true
+server 'di8905.ddns.net', user: 'deployer', roles: %w{web app db}, primary: true
 
 
 # role-based syntax
