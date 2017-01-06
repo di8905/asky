50da3412461931dbdc198fa39fc7657deec04c98
Di <pdv.spb@gmail.com>
2017-01-06 15:46:53 +0300

    Cable request origins add di8905

--- config/application.rb
+++ config/application.rb
@@ -26,6 +26,8 @@ module Asky
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.active_job.queue_adapter = :sidekiq
+    config.action_cable.allowed_request_origins = 
+    ['http://di8905.ddns.net']
     config.action_cable.disable_request_forgery_protection = false
 
     config.generators do |g|
