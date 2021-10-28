59efa6f236079cb271df6ce15f35f89cb85bd082
denis.pav <denis.pav@okwork.io>
2021-10-28 22:51:31 +0300

    Mailers coverage

--- spec/jobs/send_question_digest_job_spec.rb
+++ spec/jobs/send_question_digest_job_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SendQuestionDigestJob, type: :job do
+  let!(:user) { FactoryGirl.create(:user) }
+
   it 'sends daily digest to each user' do
     User.find_each.each { |user| expect(DailyQuestionsDigestMailer).to receive(:questions_digest).with(user).and_call_original }
     SendQuestionDigestJob.perform_now
